7902f7103eba1704026fdbabb4b6b602fbf855a5
4emix <95339041+4emix@users.noreply.github.com>
2025-10-19 20:23:05 +0300

    Add Supabase client defaults for provided project

--- lib/supabase/server-client.ts
+++ lib/supabase/server-client.ts
@@ -1,13 +1,14 @@
 import { cookies } from 'next/headers';
 import { createServerClient } from '@supabase/auth-helpers-nextjs';
 import type { Database } from '@/types/supabase';
+import { SUPABASE_ANON_KEY, SUPABASE_URL } from './config';
 
 export function createServerSupabaseClient() {
   const cookieStore = cookies();
 
   return createServerClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY,
     {
       cookies: {
         get(name: string) {
